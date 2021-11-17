86a3b286b0f331de9d62d0d4059866b156303d0a
thiago12prata <thiago12prata@gmail.com>
2021-11-17 09:55:03 -0300

    implementado FindAll e findById

--- src/main/java/com/practice/resources/UsuarioResource.java
+++ src/main/java/com/practice/resources/UsuarioResource.java
@@ -1,19 +1,33 @@
 package com.practice.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.practice.entities.Usuario;
+import com.practice.services.UsuarioService;
 
 @RestController
 @RequestMapping(value = "/usuarios")
 public class UsuarioResource {
 
+	@Autowired
+	private UsuarioService service;
+	
 	@GetMapping
-	public ResponseEntity<Usuario> findAll(){
-		Usuario u = new Usuario(1L, "thiago", "thiago@gmail.com","9 9999-9999", "123456");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<Usuario>> findAll(){
+		List<Usuario> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	private ResponseEntity<Usuario> findById(@PathVariable Long id) {
+		Usuario obj = service.findbyId(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
---
+++ src/main/java/com/practice/services/UsuarioService.java
@@ -0,0 +1,25 @@
+package com.practice.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.practice.entities.Usuario;
+import com.practice.repositories.UsuarioRepository;
+
+@Service
+public class UsuarioService {
+
+	@Autowired
+	private UsuarioRepository repository;
+	
+	public List<Usuario> findAll() {
+		return repository.findAll();
+	}
+	public Usuario findbyId(Long id) {
+		Optional<Usuario> obj =  repository.findById(id);
+		return obj.get();
+	}
+}
