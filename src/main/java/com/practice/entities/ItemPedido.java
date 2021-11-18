8f55621289e42d985ea3e8c78235a5cdd716d487
thiago12prata <thiago12prata@gmail.com>
2021-11-18 10:52:20 -0300

    Item pedido PK, e entidade item pedido

---
+++ src/main/java/com/practice/entities/ItemPedido.java
@@ -0,0 +1,69 @@
+package com.practice.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+
+import com.practice.entities.pk.ItemPedidoPK;
+
+@Entity
+public class ItemPedido implements Serializable{
+	private static final long serialVersionUID = 1L;
+
+	@EmbeddedId
+	private ItemPedidoPK id;
+	
+	private Integer quantidade;
+	private Double preco;
+	
+	public ItemPedido() {
+	}
+	public ItemPedido(Pedido pedido, Produto produto, Integer quantidade, Double preco) {
+		id.setPedido(pedido);
+		id.setProduto(produto);
+		this.quantidade = quantidade;
+		this.preco = preco;
+	}
+	public Pedido getPedido() {
+		return id.getPedido();
+	}
+	public void setPedido(Pedido pedido) {
+		id.setPedido(pedido);
+	}
+	public Produto getProduto() {
+		return id.getProduto();
+	}
+	public void setProduto(Produto produto) {
+		id.setProduto(produto);
+	}
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+	public Double getPreco() {
+		return preco;
+	}
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ItemPedido other = (ItemPedido) obj;
+		return Objects.equals(id, other.id);
+	}
+}
