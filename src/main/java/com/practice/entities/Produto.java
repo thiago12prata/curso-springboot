98aff8d89db4f94595f33e93dcd88e782e7999ed
thiago12prata <thiago12prata@gmail.com>
2021-11-18 13:21:38 -0300

    implementado metodo getPedidos na entidade produto

--- src/main/java/com/practice/entities/Produto.java
+++ src/main/java/com/practice/entities/Produto.java
@@ -12,6 +12,9 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Produto implements Serializable {
@@ -28,7 +31,10 @@ public class Produto implements Serializable {
 	@ManyToMany
 	@JoinTable(name = "produto_categoria", joinColumns = @JoinColumn(name = "produto_id"), inverseJoinColumns = @JoinColumn(name = "categoria_id"))
 	private Set<Categoria> categorias = new HashSet<>();
-
+	@OneToMany(mappedBy = "id.produto")
+	private Set<ItemPedido> itens = new HashSet<>();
+	
+	
 	public Produto() {
 	}
 
@@ -43,51 +49,48 @@ public class Produto implements Serializable {
 	public Long getId() {
 		return id;
 	}
-
 	public void setId(Long id) {
 		this.id = id;
 	}
-
 	public String getNome() {
 		return nome;
 	}
-
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-
 	public String getDescricao() {
 		return descricao;
 	}
-
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
-
 	public Double getPreco() {
 		return preco;
 	}
-
 	public void setPreco(Double preco) {
 		this.preco = preco;
 	}
-
 	public String getImgUrl() {
 		return imgUrl;
 	}
-
 	public void setImgUrl(String imgUrl) {
 		this.imgUrl = imgUrl;
 	}
-
 	public static long getSerialversionuid() {
 		return serialVersionUID;
 	}
-
 	public Set<Categoria> getCategorias() {
 		return categorias;
 	}
-
+	@JsonIgnore
+	public Set<Pedido> getPedidos() {
+		Set<Pedido> pedidos = new HashSet<>();
+		for(ItemPedido ip : itens) {
+			pedidos.add(ip.getPedido());
+		}
+		return pedidos;
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
